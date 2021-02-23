9d5f32a11dde01cc8ee1f49be8469c21768671f4
Regan Sarwas <regan_sarwas@nps.gov>
2021-02-23 11:27:36 -0900

    Fix for last commit

--- Tools/make_folders.py
+++ Tools/make_folders.py
@@ -12,6 +12,7 @@ in a work folder used previously to process topo maps.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import errno
 import os
 import shutil
 import sys
